37162379a64756346289774c43a43605e927394c
1210Disha <98633068+1210Disha@users.noreply.github.com>
2023-09-10 15:53:39 +0530

    Add files via upload

---
+++ Disha_half_yearly_9b/explanation_module.py
@@ -0,0 +1,21 @@
+"""
+Module-
+->A file containing a set of functions you want to include in your application is called a module.
+->In this file we are storing the code as a function i.e using the 'def' keyword and creating a module.
+->We will call the function for the required output.
+->Making a module saves time and later can be used in future for other purposes.
+
+
+How does the module work-
+->first using a the 'def' keyword make a function for your code.
+for example-
+def name_of_the_function():
+                        write your code
+
+->Now you can call the function.
+for example-
+from your_module_name import name_of_the_function
+name_of_the_funtion()
+"""
+
+
---
+++ Disha_half_yearly_9b/functions_index.py
@@ -0,0 +1,63 @@
+"""All the functions-"""
+
+"""reading an image and video"""
+#image
+#video
+
+"""rescaling and resizing """
+#rescale_image
+#resize_video
+
+"""Drawing shapes and putting text"""
+#blank
+#paint
+#rectangle
+#circle
+#line
+#text
+#shapes
+
+"""5 essential functions in open cv"""
+#gray
+#blur
+#edge_cascade
+#dilated
+#eroded
+
+"""image transformation """
+#translate
+#reflection_x_axis
+#reflection_y_axis
+#rotate
+#shrink
+#enlarge-
+#crop
+#x_axis_shearing
+#y_axis_shearing
+#contours-
+#color_spaces
+#Kernel_blur
+#average
+#gaussian_blur
+#median
+#bilateral
+  
+"""bitwise operations"""
+#bitwise_and
+#bitwise_or
+#bitwise_xor
+#bitwise_not
+
+"""historgram and masking """
+#masking
+#alpha_blurring
+#histogram
+
+
+
+
+
+
+
+
+
